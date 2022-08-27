40f4ef71607379a59dca9255e0272612416a44fd
Utsav Gurmachhan Magar <utsavmagar88728@gmail.com>
2022-08-27 12:29:12 +0545

    import export example

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import _ from "lodash";
+import myName from "./myName.js";
 
 function component() {
 	const element = document.createElement("div");
 
-	element.innerHTML = _.join(["Hello", "webpack"], " ");
+	element.innerHTML = _.join(["Hello", "webpack."], " ");
+	element.innerHTML += `<br>${myName("John")}`;
 
 	return element;
 }
---
+++ src/myName.js
@@ -0,0 +1,5 @@
+const myName = (name) => {
+	return "Hello, My name is " + name + ".";
+};
+
+export default myName;
