6afccb1efbe09556f038413cc99e447f40f1dc2f
Utsav Gurmachhan Magar <utsavmagar88728@gmail.com>
2022-08-27 12:50:37 +0545

    loaded images

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import _ from "lodash";
 import myName from "./myName.js";
 import "./style.css";
+import OdinIcon from "./odin-icon.svg";
 
 function component() {
 	const element = document.createElement("div");
@@ -8,6 +9,11 @@ function component() {
 	element.innerHTML = _.join(["Hello", "webpack."], " ");
 	element.innerHTML += `<br>${myName("John")}`;
 	element.classList.add("hello");
+
+	const myOdinIcon = new Image();
+	myOdinIcon.src = OdinIcon;
+
+	element.appendChild(myOdinIcon);
 
 	return element;
 }
