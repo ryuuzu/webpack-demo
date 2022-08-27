3a17012652c6f81c2a8152af83c9b625938c0c09
Utsav Gurmachhan Magar <utsavmagar88728@gmail.com>
2022-08-27 12:44:59 +0545

    loadded css

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import _ from "lodash";
 import myName from "./myName.js";
+import "./style.css";
 
 function component() {
 	const element = document.createElement("div");
 
 	element.innerHTML = _.join(["Hello", "webpack."], " ");
 	element.innerHTML += `<br>${myName("John")}`;
+	element.classList.add("hello");
 
 	return element;
 }
